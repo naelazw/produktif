d160c57277e47e69e987fd02fac059a79a24f556
naelazw <naelazahwa487@gmail.com>
2024-09-02 08:57:37 +0700

    pertama

---
+++ tipedata.php
@@ -0,0 +1,18 @@
+<?php
+$x = 12348;
+var_dump($x);
+echo "<br>";
+$x = -312; //negatif number
+var_dump($x);
+echo "<br>";
+$x = 0x8c; //hexadecimal
+var_dump($x);
+echo "<br>";
+$x = 047; //oktal
+var_dump($x);
+echo "<br>";
+$x =2.43
+
+
+
+?>
---
+++ variabel.php
@@ -0,0 +1,9 @@
+<?php
+$nim ="E41230243";
+$nama = "Naela"; 
+$prodi = "Teknik Infromatika";
+echo "Selamat Praktikum";
+echo $nim;
+echo $nama;
+echo $prodi;
+   ?>
